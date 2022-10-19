e26372eb269bf3705c1f1817300554a8a478fe5f
Remeet <114641191+Remeet@users.noreply.github.com>
2022-10-19 11:03:01 +0530

    api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import File from './Components/File';
+
+function App() {
+  return (
+    <>
+      <File/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Note.js
@@ -0,0 +1,64 @@
+/*
+for json server :-
+cmd :-
+
+npm install -g json-server
+
+json-server --watch db.json --port 3004
+*/
+
+
+/*
+vs code terminal :-
+
+npm install axios
+*/
+
+
+/*
+for json fake api :-
+
+https://jsonplaceholder.typicode.com/
+*/
+
+
+////////////////////////////////
+
+
+/*
+Detailed Note :-
+
+https://github.com/typicode/json-server
+
+  npm install from terminal in vscode
+
+npm install -g json-server
+
+  *******[IMPORTANT]*******
+  every time when you work in this project you must obey this command :-
+	open other server from cmd --->
+	open your project in cmd ---> then type --->
+
+json-server --watch db.json --port 3004
+
+***************************
+
+	then new file dg.jason created in your project file in vscode --->
+	open your link in google 
+  copy data from here
+	i.e. 
+https://jsonplaceholder.typicode.com/users
+
+	then paste date in dg.jason file in perticular format
+
+	then you can see msg in cmd --->
+	Resources
+http://localhost:3004/users
+
+	means success...
+	you have create a new localhost server...
+
+	now you can add, delete, edit in direct file (dg.jason)
+
+*/
+
